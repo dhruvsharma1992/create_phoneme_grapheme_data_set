e12ff6c374238e63556b507eefbc89257c4b03ed
saranshsharma <saransh2700@gmail.com>
2015-07-07 19:29:15 +0530

    Added Entropy Function, created a csv file

--- testscript.py
+++ testscript.py
@@ -1,5 +1,6 @@
 import pickle
 import sys
+from math import *
 from sqliteCRUD import *
 from dictionary import *
 import sqlite3 as lite
@@ -79,11 +80,66 @@ def pushToResult(ins_query):
 def parameterAnalysis(crud_obj):
     crud_obj.getParameterMapping()
     return
+def generateEntropies():
+    getProbabilities()
+    return
+
+def getProbabilities():
+    try:
+        conn,cur = getResultDB()
+        open("Entropy_list.csv","w").close()
+        with open("Entropy_list.csv","w") as f:
+            f.write("arpabet,character,1_before,2_before,3_before,1_after,2_after,3_after\n")
+        prob_stmt = 'select \"arpabet\",\"character\",(\"1_before_vowel\"/(\"1_before_vowel\"+\"1_before_consonant\"+\"1_before_hash\")) as \'p(1_before_vowel) \',(\"1_before_consonant\"/(\"1_before_vowel\"+\"1_before_consonant\"+\"1_before_hash\")) as \'p(1_before_consonant) \',(\"1_before_hash\"/(\"1_before_vowel\"+\"1_before_consonant\"+\"1_before_hash\")) as \'p(1_before_hash) \',(\"2_before_vowel\"/(\"2_before_vowel\"+\"2_before_consonant\"+\"2_before_hash\")) as \'p(2_before_vowel) \',(\"2_before_consonant\"/(\"2_before_vowel\"+\"2_before_consonant\"+\"2_before_hash\")) as \'p(2_before_consonant) \',(\"2_before_hash\"/(\"2_before_vowel\"+\"2_before_consonant\"+\"2_before_hash\")) as \'p(2_before_hash) \',(\"3_before_vowel\"/(\"3_before_vowel\"+\"3_before_consonant\"+\"3_before_hash\")) as \'p(3_before_vowel) \',(\"3_before_consonant\"/(\"3_before_vowel\"+\"3_before_consonant\"+\"3_before_hash\")) as \'p(3_before_consonant) \',(\"3_before_hash\"/(\"3_before_vowel\"+\"3_before_consonant\"+\"3_before_hash\")) as \'p(3_before_hash) \',(\"1_after_vowel\"/(\"1_after_vowel\"+\"1_after_consonant\"+\"1_after_hash\")) as \'p(1_after_vowel) \',(\"1_after_consonant\"/(\"1_after_vowel\"+\"1_after_consonant\"+\"1_after_hash\")) as \'p(1_after_consonant) \',(\"1_after_hash\"/(\"1_after_vowel\"+\"1_after_consonant\"+\"1_after_hash\")) as \'p(1_after_hash) \',(\"2_after_vowel\"/(\"2_after_vowel\"+\"2_after_consonant\"+\"2_after_hash\")) as \'p(2_after_vowel) \',(\"2_after_consonant\"/(\"2_after_vowel\"+\"2_after_consonant\"+\"2_after_hash\")) as \'p(2_after_consonant) \',(\"2_after_hash\"/(\"2_after_vowel\"+\"2_after_consonant\"+\"2_after_hash\")) as \'p(2_after_hash) \',(\"3_after_vowel\"/(\"3_after_vowel\"+\"3_after_consonant\"+\"3_after_hash\")) as \'p(3_after_vowel) \',(\"3_after_consonant\"/(\"3_after_vowel\"+\"3_after_consonant\"+\"3_after_hash\")) as \'p(3_after_consonant) \',(\"3_after_hash\"/(\"3_after_vowel\"+\"3_after_consonant\"+\"3_after_hash\")) as \'p(3_after_hash) \' from result_no_hash'
+        cur.execute(prob_stmt)
+        row = cur.fetchone()
+        while row:
+            getEntropy(str(row[0]).encode('ascii', 'ignore'),str(row[1]).encode('ascii', 'ignore'),row[2:])
+            #getEntropy(row)
+            row = cur.fetchone()
+    except Exception as e:
+        print e
+    return
+def getEntropy(arpabet, character, probs):
+    eList = ["1_before","2_before","3_before","1_after","2_after","3_after"]
+    j=0
+    #print arpabet, character
+    temp_line = ''
+    for i in range(0,len(eList)):
+        #temp_line = temp_line.join(getAccuEnt(probs[j:j+3]))
+        accu_ent = getAccuEnt(probs[j:j+3])
+        print accu_ent
+        temp_line += str(accu_ent)+","
+        #print eList[i],getAccuEnt(probs[j:j+3])
+        j+=3
+    #print arpabet,"->",character,"\n",probs[0:3],type(probs[0:4])
+    hash_str = str(arpabet)+","+str(character)+","+temp_line.strip(',')
+    print hash_str
+    with open("Entropy_list.csv","a") as f:
+        f.write(hash_str+"\n")
+    return
+def getAccuEnt(probList):
+    E = float(0)
+##    print sum(probList)
+##    print probList
+    try:
+        for x in probList:
+            if x == 0:
+                #print"found 0"
+                raise Exception("got a zero")
+            E  += x*log(x,2)
+        E = -(E)
+    except Exception as e:
+        #print "none discovered"
+        E = "NONE"
+    finally:
+        return E
 def wrappedRunner():
-	try:
-		entropyTester()
-	except Exception as e:
-		print e
+    try:
+        #entropyTester()
+        generateEntropies()
+    except Exception as e:
+        print e
 ########################
 
 wrappedRunner()
